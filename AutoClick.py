2bdaae47988cd161976b328e7aa14702229ce4df
Nokeic <147358006+Nokeic@users.noreply.github.com>
2023-11-01 09:55:43 -0300

    Update AutoClick.py

--- AutoClick.py
+++ AutoClick.py
@@ -5,10 +5,10 @@ from time import sleep
 print("Programa AutoClicker\n By Nokeic\n")
 
 tecla_de_execucao = input("Insira a tecla para o autoclicker funcionar (Terá que mate-la pressionada para o autoclick funcionar): ")
-tecla_de_parada = input("Insira a tecla para parar a execução: ")
+tecla_de_parada = input("Insira a tecla para parar a execução: ") 
 
 while True:
-    sleep(0.03)
+    sleep(0.03) # Caso for usar para "Cheat", e queira legit, mude (0.03) para (0.06)
     if keyboard.is_pressed(tecla_de_execucao):
         mouse.click()
     if keyboard.is_pressed(tecla_de_parada):
